e05fb643161a57c89bc07b55399896994b5ebd71
Blueswen <blueswen.tw@gmail.com>
2014-06-06 17:31:58 +0800

    add

--- js/script.js
+++ js/script.js
@@ -75,8 +75,9 @@ FB.getLoginStatus(function(response) {
     var canvasWidth=canvas.width;//大小
     var canvasHeight=canvas.height;//高度
     var isDragging=false;//拖拉
-    var px,py,tx,ty;//暫存photo和text的座標
-    console.log(offsetX + ' ' +offsetY)
+    var px=0,py=0,tx=200,ty=400;//暫存photo和text的座標
+    var mx,my;
+    var isStart=false;
 
     function handleMouseDown(e){//滑鼠按下的函數
       canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
@@ -96,12 +97,18 @@ FB.getLoginStatus(function(response) {
       canMouseX=parseInt(e.clientX-offsetX);
       canMouseY=parseInt(e.clientY-offsetY);
       // user has left the canvas, so clear the drag flag
-      //isDragging=false;
+      isDragging=false;
     }
 
     function handleMouseMove(e){//滑鼠移動的event
       canMouseX=parseInt(e.clientX-offsetX);
       canMouseY=parseInt(e.clientY-offsetY);
+      // if doesn't start darw image to default position
+      if(!isStart){
+      	ctx.drawImage(profileIMG,px,py);//從XY軸0，0值開始畫如profileimg
+				ctx.drawImage(img3,tx,ty); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+				ctx.drawImage(img2,0,0); //劃入img2
+      }
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
       	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
@@ -110,15 +117,15 @@ FB.getLoginStatus(function(response) {
 				//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 				//canvas.height = profileIMG.height;		
 				if($('#target').val()=='photo'){
-					px = canMouseX;
-					py = canMouseY;
+					px = canMouseX-mx;
+					py = canMouseY-my;
 					ctx.drawImage(profileIMG,px,py);//從XY軸0，0值開始畫如profileimg
 					ctx.drawImage(img3,tx,ty); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 					ctx.drawImage(img2,0,0); //劃入img2
 				}
 				else if($('#target').val()=='txt'){
-					tx = canMouseX;
-					ty = canMouseY;
+					tx = canMouseX-mx;
+					ty = canMouseY-my;
 					ctx.drawImage(profileIMG,px,py);//從XY軸0，0值開始畫如profileimg
 					ctx.drawImage(img3,tx,ty); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 					ctx.drawImage(img2,0,0); //劃入img2
@@ -126,7 +133,9 @@ FB.getLoginStatus(function(response) {
 				var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 				ctx.fillStyle = "black"; //字體顏色
 				ctx.font='20px "微軟正黑體"'; //字體大小和字形
-				ctx.fillText(inputedText, tx+80,ty+40); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+				ctx.fillText(inputedText, tx+75,ty+45); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+				mx = canMouseX;
+				my = canMouseY;
       }
     }
 
