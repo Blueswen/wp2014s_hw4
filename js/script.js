0184f344220f33b7f95101748d4017749918a272
Blueswen <blueswen.tw@gmail.com>
2014-06-06 11:03:33 +0800

    add pos

--- js/script.js
+++ js/script.js
@@ -73,6 +73,7 @@ FB.getLoginStatus(function(response) {
     var canvasWidth=canvas.width;//大小
     var canvasHeight=canvas.height;//高度
     var isDragging=false;//拖拉
+    var px,py,tx,ty;//暫存photo和text的座標
 
     function handleMouseDown(e){//滑鼠按下的函數
       canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
@@ -100,15 +101,25 @@ FB.getLoginStatus(function(response) {
       canMouseY=parseInt(e.clientY-offsetY);
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
-      	console.log($('#target').val());
       	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 				var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
 				profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 				//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
-				//canvas.height = profileIMG.height;
-				ctx.drawImage(profileIMG,canMouseX-profileIMG.width/2,canMouseY-profileIMG.height/2);//從XY軸0，0值開始畫如profileimg
-				ctx.drawImage(img3,0,0); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-				ctx.drawImage(img2,0,0); //劃入img2
+				//canvas.height = profileIMG.height;		
+				if($('#target').val()=='photo'){
+					px = canMouseX-profileIMG.width/2;
+					py = canMouseY-profileIMG.height/2;
+					ctx.drawImage(profileIMG,px,py);//從XY軸0，0值開始畫如profileimg
+					ctx.drawImage(img3,tx,ty); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+					ctx.drawImage(img2,0,0); //劃入img2
+				}
+				else if($('#target').val()=='txt'){
+					tx = canMouseX-199/2;
+					ty = canMouseY-89/2;
+					ctx.drawImage(profileIMG,px,py);//從XY軸0，0值開始畫如profileimg
+					ctx.drawImage(img3,tx,ty); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+					ctx.drawImage(img2,0,0); //劃入img2
+				}
 				var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 				ctx.fillStyle = "black"; //字體顏色
 				ctx.font='20px "微軟正黑體"'; //字體大小和字形
